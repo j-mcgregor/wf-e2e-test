90e3ab9443460482fa43350d96edbefbec47a266
loyd0 <samloyd@hotmail.co.uk>
2021-08-16 16:38:33 +0100

    update base components

--- components/layout/Footer.js
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <footer>Antler Digital Starter Template</footer>;
-};
-
-export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 export default function Home(props) {
   return <div>Wiserfunding's new platform</div>;
 }
-export async function getStaticProps({ _preview, _params }) {
+export async function getStaticProps({ _preview, _params, locale }) {
   return {
     props: {},
-
-    revalidate: 10
+    // You can get the messages from anywhere you like, but the recommended
+    // pattern is to put them in JSON files separated by language and read
+    // the desired one based on the `locale` received from Next.js.
+    messages: {
+      // eslint-disable-next-line security/detect-non-literal-require
+      ...require(`../messages/${locale}/index.${locale}.json`)
+    }
   };
 }
