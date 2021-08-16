96eac24790117c4f0514ee712b3b01c059b4942c
loyd0 <samloyd@hotmail.co.uk>
2021-08-16 17:08:10 +0100

    updated index

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Layout from '../components/layout/Layout';
+
+export default function Home() {
+  return <Layout title="Dashboard" >Wiserfunding's new platform</Layout>;
+}
+export async function getStaticProps({ locale }: {locale: {}}) {
+  return {
+    props: {},
+    // You can get the messages from anywhere you like, but the recommended
+    // pattern is to put them in JSON files separated by language and read
+    // the desired one based on the `locale` received from Next.js.
+    messages: {
+      // eslint-disable-next-line security/detect-non-literal-require
+      ...require(`../messages/${locale}/index.${locale}.json`)
+    }
+  };
+}
