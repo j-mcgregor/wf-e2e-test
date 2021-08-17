57f440cd1241e923714a5ca07ca02b5caefc97a1
dnirns <danielirons@protonmail.com>
2021-08-17 11:16:14 +0100

    dan test commit

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -3,22 +3,21 @@ import { useRouter } from 'next/router';
 import Nav from './Nav';
 import Seo from './Seo';
 
-
-type LayoutProps = {
-  children: React.ReactNode
-  noNav?: boolean | undefined
-  title?: string
-  description?: string
-}
+// type LayoutProps = {
+//   children: React.ReactNode
+//   noNav?: boolean | undefined
+//   title?: string
+//   description?: string
+// }
 
 const Layout = ({ children, noNav, title, description }: LayoutProps) => {
   const router = useRouter();
-  const path: string  = router.pathname
+  const path: string = router.pathname;
 
   return (
     <div>
       <Seo title={title} description={description} path={path} />
-      { !noNav && <Nav path={path} /> }
+      {!noNav && <Nav path={path} />}
       <main className="relative">{children}</main>
     </div>
   );
