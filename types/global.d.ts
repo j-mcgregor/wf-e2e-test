b4417e82dd73d7bfb0807a81f8b14f588cf39f08
loyd0 <samloyd@hotmail.co.uk>
2021-08-23 18:40:46 +0100

    updated package on relative time to one with type support

---
+++ types/global.d.ts
@@ -0,0 +1,7 @@
+import React, { ReactElement, ReactNodeArray, JSXElementConstructor} from 'react';
+
+
+export type TranslateInput = string | ReactNodeArray | ReactElement<any, string | JSXElementConstructor<any>>
+
+
+export type StatDataType = string | number | Date 
