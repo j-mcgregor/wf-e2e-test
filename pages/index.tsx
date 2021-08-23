51815a203ae14ca220777707476d80a5d0cfd91d
loyd0 <samloyd@hotmail.co.uk>
2021-08-23 19:07:25 +0100

    added structural layout for dashboard and updated lang model to reflect additions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import { useTranslations } from 'next-intl';
 import { GetStaticPropsContext } from 'next';
-import { useSession } from 'next-auth/client';
 import Layout from '../components/layout/Layout';
-import LinkCard from '../components/elements/LinkCard';
+import LinkCard from '../components/cards/LinkCard';
 import {
   LightningBoltIcon,
   SearchCircleIcon,
@@ -18,33 +17,52 @@ import React from 'react';
 import { useRecoilValue } from 'recoil';
 import initialAppState from '../lib/appState';
 
-
 export default function Dashboard() {
-  const [session, loading] = useSession();
-
   const t = useTranslations();
 
-  const { user } = useRecoilValue(initialAppState)
-
+  const { user } = useRecoilValue(initialAppState);
 
   return (
     <Layout title="Dashboard">
-      {session ? `Logged in as ${session?.user?.name}` : 'Not Logged in'}
+      <div className="grid grid-cols-7 gap-x-4 text-primary">
+        <div className=" col-span-5 ">
+          <div>
+            <p className="text-base h-6 -mt-6">{t('welcome back')}</p>
 
-      <Stats
-        stats={[
-          { header: t('total reports'), data: user?.recent_usage?.reports_ran },
-          { header: t('api requests'), data: user?.recent_usage?.api_requests },
-          {
-            header: t('last login'),
-            data: user?.recent_usage?.api_requests,
-            timeAgo: true
-          }
-        ]}
-      />
+            <div className="flex justify-between">
+            <h1 className="text-2xl font-semibold">{user && user.name}</h1>
+            <p className="font-semibold">{t('last 7 days')}</p>
+            </div>
+          
+          </div>
+          <Stats
+            stats={[
+              {
+                header: t('total reports'),
+                data: user?.recent_usage?.reports_ran
+              },
+              {
+                header: t('api requests'),
+                data: user?.recent_usage?.api_requests
+              },
+              {
+                header: t('last login'),
+                data: user?.recent_usage?.api_requests,
+                timeAgo: true
+              }
+            ]}
+          />
+
+          {/* Report table goes here */}
+        </div>
+        <div className="col-span-2">
+          <h3 className="font-semibold">{t('updates')}</h3>
+          {/* Twitter page goes here */}
+        </div>
+      </div>
 
       {/* link cards */}
-      <div className="flex w-full">
+      <div className="grid xl:grid-cols-4 grid-cols-2 gap-4 mt-8">
         <LinkCard
           icon={<LightningBoltIcon className='className="h-6 w-6 text-white' />}
           iconColor="bg-highlight"
