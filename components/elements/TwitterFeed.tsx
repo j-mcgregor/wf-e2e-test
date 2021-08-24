6dc9c69a93e3429b33798e9b0bba47f0d6173cb5
loyd0 <samloyd@hotmail.co.uk>
2021-08-24 20:05:07 +0100

    update twitterfeed to use package for SSR issues

--- components/elements/TwitterFeed.tsx
+++ components/elements/TwitterFeed.tsx
@@ -1,18 +1,17 @@
-const TwitterFeed = () => {
+import { Timeline } from 'react-twitter-widgets';
+
+const TwitterFeed = ({ className }: { className?: string }) => {
   return (
-    <div className="my-10 shadow">
-      <a
-        className="twitter-timeline"
-        data-height="500"
-        href="https://twitter.com/wiserfunding?ref_src=twsrc%5Etfw"
-      >
-        Tweets by wiserfunding
-      </a>
-      <script
-        async
-        src="https://platform.twitter.com/widgets.js"
-        charSet="utf-8"
-      ></script>
+    <div
+      className={`shadow bg-white ${className} overflow-hidden rounded h-[400px]`}
+    >
+      <Timeline
+        dataSource={{
+          sourceType: 'profile',
+          screenName: 'Wiserfunding'
+        }}
+        options={{ height: 400 }}
+      />
     </div>
   );
 };
