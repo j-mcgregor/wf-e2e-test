860adfc5d03a8662eb4f7d49380da1d513905af8
loyd0 <samloyd@hotmail.co.uk>
2021-08-30 10:08:18 +0100

    added wrapper for animation pulse

--- components/elements/TwitterFeed.tsx
+++ components/elements/TwitterFeed.tsx
@@ -3,15 +3,21 @@ import { Timeline } from 'react-twitter-widgets';
 const TwitterFeed = ({ className }: { className?: string }) => {
   return (
     <div
-      className={`shadow bg-white overflow-hidden rounded ${className} `}
+      className={`relative shadow bg-white overflow-hidden rounded ${className} `}
     >
+      <div className="animate-pulse bg-gray-300 h-full w-full relative -z-10" />
+
+      <div className="absolute z-10 h-full w-full top-0">
       <Timeline
         dataSource={{
           sourceType: 'profile',
           screenName: 'Wiserfunding'
         }}
+
         options={{ height: 402 }}
       />
+      </div>
+     
     </div>
   );
 };
