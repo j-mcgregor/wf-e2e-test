c052c26cc6144991535c6405b6c8aeccc01929f0
dnirns <danielirons@protonmail.com>
2021-08-30 11:36:30 +0100

    added small logo & made responsive

--- components/elements/WFLogo.tsx
+++ components/elements/WFLogo.tsx
@@ -1,17 +1,30 @@
-import Image  from 'next/image';
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 import WiserfundingLogo from '../../public/images/logos/wiserfunding-logo.svg';
+import WFLogoSmall from '../../public/images/logos/wf-logo.svg';
 
 const WFLogo = () => {
   return (
-    <Image
-    alt={'Wiserfunding Logo'}
-    layout="fill"
-    objectFit="contain"
-    src={WiserfundingLogo}
-  />
-  )
-}
+    <>
+      <div className="hidden lg:block">
+        <Image
+          alt={'Wiserfunding Logo'}
+          layout="fill"
+          objectFit="contain"
+          src={WiserfundingLogo}
+        />
+      </div>
+      <div className="lg:hidden">
+        <Image
+          alt={'Wiserfunding Logo'}
+          layout="fill"
+          objectFit="contain"
+          src={WFLogoSmall}
+        />
+      </div>
+    </>
+  );
+};
 
-export default WFLogo
+export default WFLogo;
