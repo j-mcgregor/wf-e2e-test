ae78bd102eeb9899cf2d9459f41aad0d5e29dcf7
loyd0 <samloyd@hotmail.co.uk>
2021-09-01 10:12:25 +0100

    pre-dave settings

--- pages/report/[...id].tsx
+++ pages/report/[...id].tsx
@@ -1,3 +1,4 @@
+/* eslint-disable security/detect-non-literal-require */
 import { GetServerSidePropsContext } from 'next';
 import React from 'react';
 
@@ -40,8 +41,8 @@ export const getServerSideProps = getServerSidePropsWithAuth(
           // You can get the messages from anywhere you like, but the recommended
           // pattern is to put them in JSON files separated by language and read
           // the desired one based on the `locale` received from Next.js.
-          // eslint-disable-next-line security/detect-non-literal-require
-          ...require(`../../messages/${locale}/report.${locale}.json`)
+          ...require(`../../messages/${locale}/report.${locale}.json`),
+          ...require(`../../messages/${locale}/general.${locale}.json`)
         }
       }
     };
