ad94ec63095fdc921b93a786c3c3633ea7fff4ab
loyd0 <samloyd@hotmail.co.uk>
2021-09-09 18:37:18 +0100

    updated summary details to be prop-centric and sorted time issues

--- components/report-sections/summary/SummaryDetails.tsx
+++ components/report-sections/summary/SummaryDetails.tsx
@@ -1,42 +1,52 @@
 import { useTranslations } from 'use-intl';
 import { SummaryInfo } from '../../../types/report';
 
-interface SummaryDetails {
-  info: SummaryInfo;
-}
-
-const SummaryDetails = ({ info }: SummaryDetails) => {
+const SummaryDetails = ({
+  regNumber,
+  sector,
+  incorporationDate,
+  lastAccountDate,
+  description
+}: SummaryInfo) => {
   const t = useTranslations();
+
+  
 
   return (
     <div className="flex flex-col bg-white border shadow-sm rounded h-full">
       <div className="flex justify-between h-1/4 border-b border-bg ">
         <div className="border-r border-bg p-3 w-1/2 flex flex-col justify-center">
           <p className="py-1 text-primary">{t('registration number')}</p>
-          <p className="py-1 font-bold ">{info?.regNumber}</p>
+          <p className="py-1 font-bold ">{regNumber}</p>
         </div>
 
         <div className="p-3 w-1/2 flex flex-col justify-center">
           <p className="py-1 text-primary">{t('industry sector')}</p>
-          <p className="py-1 font-bold">{info?.sector}</p>
+          <p className="py-1 font-bold">{sector}</p>
         </div>
       </div>
 
       <div className="flex justify-between border-b border-bg h-1/4">
         <div className="border-r border-bg p-3 w-1/2 flex flex-col justify-center">
           <p className="py-1 text-primary">{t('incorporation date')}</p>
-          <p className="py-1 font-bold">{info?.incorporationDate}</p>
+          <p className="py-1 font-bold">
+            {new Date(incorporationDate).toLocaleDateString('en-GB', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric'
+            })}
+          </p>
         </div>
 
         <div className="p-3 w-1/2 flex flex-col justify-center">
           <p className="py-1">{t('last filed date')}</p>
-          <p className="py-1 font-bold text-primary">{info?.lastAccountDate}</p>
+          <p className="py-1 font-bold text-primary">{lastAccountDate}</p>
         </div>
       </div>
 
       <div className="p-3 flex flex-col justify-start first-line:border h-1/2">
         <p className="py-2 text-primary">{t('company description')}</p>
-        <p>{info?.description}</p>
+        <p>{description}</p>
       </div>
     </div>
   );
