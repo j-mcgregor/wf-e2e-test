1ed55e4796bdfdf72af3d2b005c0662c869c3a5a
loyd0 <samloyd@hotmail.co.uk>
2021-09-09 12:07:15 +0100

    fix build bug contact summary type

--- components/report-sections/summary/SummaryMap.tsx
+++ components/report-sections/summary/SummaryMap.tsx
@@ -3,10 +3,10 @@ import { useTranslations } from 'use-intl';
 
 import { SummaryContact } from '../../../types/report';
 
-const SummaryMap = ( props: SummaryContact) => {
+const SummaryMap = ({ contact }: { contact: SummaryContact}) => {
   const t = useTranslations();
 
-  const phone = props?.phone_numbers[0].replace(/[A-z-]/g, '');
+  const phone = contact?.phone_numbers[0].replace(/[A-z-]/g, '');
 
   // couldn't get working in a .env - API key through Dan's Google account:
   const MAPS_API_KEY = 'AIzaSyBseaTSNK34Z11NPGydJsDTzTkE1Pe9fKo';
@@ -19,35 +19,35 @@ const SummaryMap = ( props: SummaryContact) => {
           height="100%"
           loading="lazy"
           url={`https://www.google.com/maps/embed/v1/place?key=${MAPS_API_KEY}
-          &q=${props?.postal_code}}&zoom=12`}
+          &q=${contact?.postal_code}}&zoom=12`}
         />
       </div>
       <div className="flex flex-col p-4">
         <div>
           <p>{t('registered address')}</p>
           <div className="font-bold pb-2">
-            <p>{props?.address_line_1}</p>
-            <p>{props?.address_line_2}</p>
-            <p>{props?.address_line_3}</p>
-            <p>{props?.address_line_4}</p>
-            <p>{props?.postal_code}</p>
+            <p>{contact?.address_line_1}</p>
+            <p>{contact?.address_line_2}</p>
+            <p>{contact?.address_line_3}</p>
+            <p>{contact?.address_line_4}</p>
+            <p>{contact?.postal_code}</p>
           </div>
         </div>
         <div className="flex justify-between">
           <p>{t('email')}</p>
 
-          {/* {props?.emails && <p>{props.emails[0]}</p>} */}
+          {/* {contact?.emails && <p>{contact.emails[0]}</p>} */}
           <p className="font-bold">info@writeglazing.com</p>
         </div>
         <div className="flex justify-between">
           <p>{t('email')}</p>
-          {props?.websites && (
-            <p className="font-bold">{props.websites[0]}</p>
+          {contact?.websites && (
+            <p className="font-bold">{contact.websites[0]}</p>
           )}
         </div>
         <div className="flex justify-between">
           <p>{t('telephone')}</p>
-          {props?.phone_numbers && <p className="font-bold">{phone}</p>}
+          {contact?.phone_numbers && <p className="font-bold">{phone}</p>}
         </div>
       </div>
     </div>
