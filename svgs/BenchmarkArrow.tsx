c7eece7982f209b7842ede4878c793844ce0fe24
loyd0 <samloyd@hotmail.co.uk>
2021-09-14 11:53:14 +0100

    add speedo svgs

---
+++ svgs/BenchmarkArrow.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const BenchmarkArrow = () => {
+  return (
+    <svg
+      width="218"
+      height="218"
+      viewBox="0 0 218 218"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="109"
+        cy="109"
+        r="109"
+        transform="rotate(90 109 109)"
+        fill="#C4C4C4"
+        fillOpacity="0.01"
+      />
+      <path
+        d="M206.012 24.2533L194.24 11.9886C194.24 11.9886 187.862 29.8929 188.208 30.2536C188.554 30.6144 206.012 24.2533 206.012 24.2533Z"
+        fill="#3199FA"
+      />
+    </svg>
+  );
+};
+
+export default BenchmarkArrow;
---
+++ svgs/SpeedoArrow.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SpeedoArrow = () => {
+  return (
+    <svg
+      width="218"
+      height="218"
+      viewBox="0 0 218 218"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M116 114C116 114 109.221 38 108.796 38C108.371 38 102 114 102 114L102.367 114.465C105.906 118.947 112.775 118.713 116 114Z"
+        fill="#022D45"
+      />
+    </svg>
+  );
+};
+
+export default SpeedoArrow;
