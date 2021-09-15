5107dcd456dc5a66488a5f586faf5b6b599afd31
davidmitten <davidmitten88@gmail.com>
2021-09-15 14:21:40 +0100

    Finished up CommunicationForm.tsx, created corresponding messages file

--- components/forms/settings/CommunicationForm.tsx
+++ components/forms/settings/CommunicationForm.tsx
@@ -4,6 +4,7 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import CheckboxInput from '../../elements/Checkbox';
 import { SettingsSectionHeader } from '../../elements/Headers';
 import { useTranslations } from 'next-intl';
+import ErrorMessage from '../../elements/ErrorMessage';
 
 interface CommunicationFormInput {
   comments: string;
@@ -12,12 +13,11 @@ interface CommunicationFormInput {
 }
 
 const CommunicationForm = () => {
-
-  const t = useTranslations()
+  const t = useTranslations();
 
   const { register, handleSubmit, formState } =
     useForm<CommunicationFormInput>();
-  const { isDirty, isValid } = formState;
+  const { isDirty, isValid, errors } = formState;
   // eslint-disable-next-line no-console
   const onSubmit: SubmitHandler<CommunicationFormInput> = data =>
     // eslint-disable-next-line no-console
@@ -28,49 +28,58 @@ const CommunicationForm = () => {
       <div className="shadow sm:rounded-md sm:overflow-hidden">
         <div className="bg-white py-6 px-4 space-y-6 sm:p-6">
           <div>
-            <SettingsSectionHeader text={t('communication')}/>
+            <SettingsSectionHeader text={t('communication')} />
             <p className="mt-1 text-sm text-gray-500">
-              Change or update your personal information
+              {t('Change or update your personal information')}
             </p>
           </div>
 
           <fieldset>
             <legend className="text-base font-medium text-gray-900">
-              By Email
+              {t('By Email')}
             </legend>
             <div className="mt-4 space-y-4">
               <div className="flex items-center">
                 <CheckboxInput
-                  label={'Batch report completion'}
+                  label={t('Batch report completion')}
                   id={'comments'}
-                  paragraph={
+                  paragraph={t(
                     'Get notified when a batch report has completed all reports.'
-                  }
+                  )}
                   {...{ ...register('comments') }}
                   name={'comments'}
                 />
+                {errors.comments && (
+                  <ErrorMessage text={`${t('errors.comments')}`} />
+                )}
               </div>
               <div className="flex items-center">
                 <CheckboxInput
-                  label={'Service Updates'}
+                  label={t('Service Updates')}
                   id={'candidates'}
-                  paragraph={
+                  paragraph={t(
                     'Get the latest updates on the Wiserfunding platform.'
-                  }
+                  )}
                   {...{ ...register('candidates') }}
                   name={'candidates'}
                 />
+                {errors.candidates && (
+                  <ErrorMessage text={`${t('errors.candidates')}`} />
+                )}
               </div>
               <div className="flex items-center">
                 <CheckboxInput
-                  label={'Company Updates'}
+                  label={t('Company Updates')}
                   id={'offers'}
-                  paragraph={
+                  paragraph={t(
                     'Get all the latest updates relating to Wiserfunding.'
-                  }
+                  )}
                   {...{ ...register('offers') }}
                   name={'offers'}
                 />
+                {errors.offers && (
+                  <ErrorMessage text={`${t('errors.offers')}`} />
+                )}
               </div>
             </div>
           </fieldset>
@@ -84,7 +93,7 @@ const CommunicationForm = () => {
                         text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2
                         focus:ring-offset-2 focus:ring-indigo-600"
           >
-            Save
+            {t('Save')}
           </button>
         </div>
       </div>
