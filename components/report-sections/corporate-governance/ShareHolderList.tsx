8c50bfbd911b839b228108800415babba7f8a8dc
loyd0 <samloyd@hotmail.co.uk>
2021-09-23 14:09:48 +0100

    add key for shareholders

--- components/report-sections/corporate-governance/ShareHolderList.tsx
+++ components/report-sections/corporate-governance/ShareHolderList.tsx
@@ -14,9 +14,10 @@ const ShareHolderList = ({ shareholders }: ShareHolderListProps) => {
       <p className="text-xl py-4">{t('all shareholders')}</p>
 
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {shareholders.map(shareholder => {
+        {shareholders.map((shareholder, index) => {
           return (
             <ShareHolderCard
+              key={shareholder.first_name + index}
               firstName={shareholder.first_name}
               lastName={shareholder.last_name}
               linkedin={shareholder.linkedin}
