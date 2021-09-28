630d442022bac4f1adae31722eb71f3ee19f63f6
loyd0 <samloyd@hotmail.co.uk>
2021-09-28 16:12:21 +0100

    removed warnings about recoil state

--- next.config.js
+++ next.config.js
@@ -11,3 +11,16 @@ module.exports = {
     domains: ['images.unsplash.com']
   }
 };
+
+const intercept = require("intercept-stdout")
+
+// safely ignore recoil stdout warning messages 
+function interceptStdout(text) {
+  if (text.includes('Duplicate atom key')) {
+    return ''
+  }
+  return text
+}
+
+// Intercept in dev and prod
+intercept(interceptStdout)
