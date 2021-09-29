86c33bf3a1903ddb3908f06e066e620f48c9cd42
loyd0 <samloyd@hotmail.co.uk>
2021-09-29 15:58:47 +0100

    fix: 🐛 console warnings for telementry and sentry stats
    reduced console warnings on the MSW package

--- __mocks__/service-worker/index.ts
+++ __mocks__/service-worker/index.ts
@@ -1,7 +1,17 @@
 if (typeof window === 'undefined') {
   const { server } = require('./server');
-  server.listen();
+  // handle the annoying sentry and telemetry warnings
+  server.listen({
+    onUnhandledRequest: ({ method, url }: { method: string; url: URL}) => {
+      if (!url.pathname.startsWith('/api')) {
+        throw new Error(`Unhandled ${method} request to ${url}`);
+      }
+    }
+  });
 } else {
   const { worker } = require('./browser');
   worker.start();
 }
+
+// default to stop linter
+export {}
