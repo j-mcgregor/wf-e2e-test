7c4a6a21efbcbccd6746def78395965a7256ac75
dnirns <danielirons@protonmail.com>
2021-10-06 12:30:41 +0100

    feat: 🎸 drag and drop file upldoad working in uploadNewData

--- components/uploads/UploadFile.tsx
+++ components/uploads/UploadFile.tsx
@@ -1,15 +1,52 @@
-import { UploadIcon } from '@heroicons/react/outline';
+import { useState } from 'react';
+import { UploadIcon, XIcon } from '@heroicons/react/outline';
 import { TranslateInput } from '../../types/global';
 
 interface UploadFileProps {
   linkText: TranslateInput;
   text: TranslateInput;
+  fileName: string | null;
   selectFile: (e: React.FormEvent<HTMLInputElement>) => void;
+  removeFile: () => void;
+  readFile: (file: File | null) => void;
 }
 
-const UploadFile = ({ text, linkText, selectFile }: UploadFileProps) => {
+const UploadFile = ({
+  text,
+  linkText,
+  selectFile,
+  removeFile,
+  fileName,
+  readFile
+}: UploadFileProps) => {
+  const [draggedOver, setIsDraggedOver] = useState(false);
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+    setIsDraggedOver(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDraggedOver(false);
+  };
+
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    const file = e.dataTransfer.items[0].getAsFile();
+    // console.log('file from drop:');
+    readFile(file);
+    setIsDraggedOver(false);
+  };
+
   return (
-    <div className="bg-bg w-full h-52 flex flex-col items-center justify-center">
+    <div
+      onDragOver={e => handleDragOver(e)}
+      onDrop={e => handleDrop(e)}
+      onDragLeave={handleDragLeave}
+      className={`${
+        draggedOver ? 'border-highlight' : 'border-transparent'
+      } bg-bg w-full h-52 flex flex-col items-center justify-center border-4 border-opacity-80 rounded`}
+    >
       <UploadIcon className="h-20 w-20 border-2 border-primary p-4 rounded" />
 
       <form className="py-4 text-sm">
@@ -31,6 +68,15 @@ const UploadFile = ({ text, linkText, selectFile }: UploadFileProps) => {
           <span>&nbsp;{text}</span>
         </div>
       </form>
+      {fileName && (
+        <div className="flex items-center justify-center">
+          <p className="text-sm font-semibold">{fileName}</p>
+          <XIcon
+            onClick={removeFile}
+            className="h-4 w-4 font-bold ml-3 cursor-pointer"
+          />
+        </div>
+      )}
     </div>
   );
 };
--- components/uploads/UploadNewData.tsx
+++ components/uploads/UploadNewData.tsx
@@ -4,32 +4,32 @@ import Button from '../elements/Button';
 import UploadFile from './UploadFile';
 import { XIcon, CheckIcon } from '@heroicons/react/outline';
 
-type FileType = Blob | File | null;
+type FileType = {
+  type: string | null;
+  name: string | null;
+};
 type FileContentType = string | ArrayBuffer | null | undefined;
 
 type FileValidationType = {
   hasCorrectFields: boolean;
   valuesAreValid: boolean | undefined;
   isUploaded?: boolean;
+  isCSV: boolean;
 };
 
 const UploadNewData = () => {
-  // const [selectedFile, setSelectedFile] = useState<FileType>(null);
+  const [selectedFile, setSelectedFile] = useState<FileType>({
+    type: null,
+    name: null
+  });
 
-  const [fileContent, setFileContent] = useState<FileContentType>(null);
-
-  const [isCSV, setIsCSV] = useState(false);
-
-  const [valuesAreValid, setValuesAreValid] = useState<boolean | undefined>(
-    false
-  );
-
-  const [isUploaded, setIsUploaded] = useState(false);
+  const [fileContent, setFileContent] = useState<FileContentType | null>(null);
 
   const [validation, setValidation] = useState<FileValidationType>({
     hasCorrectFields: false,
     valuesAreValid: false,
-    isUploaded: false
+    isUploaded: false,
+    isCSV: false
   });
 
   const validHeaders = [
@@ -47,48 +47,65 @@ const UploadNewData = () => {
   ];
 
   useEffect(() => {
-    getValidations(fileContent);
+    getValidations(fileContent, selectedFile);
   }, [fileContent]);
 
   const handleSelectFile = (e: any) => {
-    const file = e.target.files[0];
+    const file = e && e.target.files[0];
     readFile(file);
   };
 
-  const readFile = (file: any) => {
+  const handleRemoveFile = () => {
+    setFileContent(null);
+    setValidation({
+      hasCorrectFields: false,
+      valuesAreValid: false,
+      isUploaded: false,
+      isCSV: false
+    });
+    setSelectedFile({
+      type: null,
+      name: null
+    });
+  };
+
+  const readFile = (file: File | null) => {
+    file && setSelectedFile(file);
+    // console.log('file content from button');
+    // console.log(file);
     const reader = new FileReader();
     reader.onload = function (file) {
       setFileContent(file.target?.result);
     };
-    reader.readAsText(file);
-    file.type === 'text/csv' ? setIsCSV(true) : setIsCSV(false);
+    file && reader.readAsText(file);
   };
 
-  const getValidations = (content: FileContentType) => {
+  const getValidations = (content: FileContentType, selectedFile: FileType) => {
     const str = content?.toString();
-    // split each line of CSV into array
     const headers: string[] | undefined = str?.split('\n')[0].split(',');
     const values: string[] | undefined = str?.split('\n')[1].split(',');
-    // compare each array to validate if csv headers are a subset of the valid headers array
     const isSubset = validHeaders.every(val => headers?.includes(val));
-    // create object & keys from headers and values arrays
-    const object =
+
+    const contentObject =
       values &&
       headers?.reduce((acc, curr, i) => ({ ...acc, [curr]: values[i] }), {});
 
     const hasValidValues =
-      object &&
-      requiredValues.filter(key => object[key]).length ===
+      contentObject &&
+      requiredValues.filter(key => contentObject[key]).length ===
         requiredValues.length;
+
+    const hasUploaded = false;
 
     setValidation({
       hasCorrectFields: isSubset,
-      valuesAreValid: hasValidValues
-      // isUploaded: true
+      valuesAreValid: hasValidValues,
+      isCSV: selectedFile.type === 'text/csv',
+      isUploaded: hasUploaded
     });
   };
 
-  const validationCheck = (value: boolean | undefined) => {
+  const renderValidationCheck = (value: boolean | undefined) => {
     if (value === true) {
       return <CheckIcon className="w-6 h-6 text-green-500" />;
     } else {
@@ -111,25 +128,28 @@ const UploadNewData = () => {
           text={t('or_drag_and_drop_it')}
           linkText={t('upload_your_csv')}
           selectFile={handleSelectFile}
+          readFile={readFile}
+          removeFile={handleRemoveFile}
+          fileName={selectedFile.name}
         />
 
         <div className="text-sm flex flex-col w-full items-center">
           <div>
             <p className="font-bold py-2">{t('valid_csv_check')}</p>
             <div className="flex py-1 items-center">
-              {validationCheck(validation.isUploaded)}
+              {renderValidationCheck(validation.isUploaded)}
               <p className="px-2">{t('upload_csv')}</p>
             </div>
             <div className="flex py-1 items-center">
-              {validationCheck(validation.hasCorrectFields)}
+              {renderValidationCheck(validation.hasCorrectFields)}
               <p className="px-2">{t('has_correct_fields')}</p>
             </div>
             <div className="flex py-1">
-              {validationCheck(validation.valuesAreValid)}
+              {renderValidationCheck(validation.valuesAreValid)}
               <p className="px-2">{t('matches_template')}</p>
             </div>
             <div className="flex py-1">
-              {validationCheck(isCSV)}
+              {renderValidationCheck(validation.isCSV)}
               <p className="px-2">{t('is_valid_csv')}</p>
             </div>
           </div>
