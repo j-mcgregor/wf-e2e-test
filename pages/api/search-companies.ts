af7749865bd3b55a5513d4a5d9421f5be90a6e5c
loyd0 <samloyd@hotmail.co.uk>
2021-10-20 16:33:47 +0400

    refactor: 💡 remove Companies House Api code
    Dan can continue with the mock for now before we switch to the companies
    house API (requires the API key)

--- pages/api/search-companies.ts
+++ pages/api/search-companies.ts
@@ -30,57 +30,66 @@ const SearchCompanies = async (
   // extract search query
   const searchQuery: string = request.query.query?.toString()?.toLowerCase();
 
-  // country code required for adding different jurisdictions
-  const countryCode: string = request.query?.country?.toString()?.toLowerCase();
+  // // country code required for adding different jurisdictions
+  // const countryCode: string = request.query?.country?.toString()?.toLowerCase();
 
-  if (!countryCode) {
-    return response
-      .status(404)
-      .json({
-        error: COUNTRY_CODE_REQUIRED,
-        message:
-          'Please provide a country code in order to correctly access the right database.'
-      });
-  }
+  // if (!countryCode) {
+  //   return response
+  //     .status(404)
+  //     .json({
+  //       error: COUNTRY_CODE_REQUIRED,
+  //       message:
+  //         'Please provide a country code in order to correctly access the right database.'
+  //     });
+  // }
 
   if (!searchQuery) {
     return response.status(200).json([]);
   }
 
-  if (countryCode === 'gb') {
-    const searchResults = await Company.searchUKCompaniesHouse(
-      process.env.COMPANIES_HOUSE_BASE64_KEY,
-      `companies?q=${searchQuery}`
+  // if (countryCode === 'gb') {
+  //   const searchResults = await Company.searchUKCompaniesHouse(
+  //     process.env.COMPANIES_HOUSE_BASE64_KEY,
+  //     `companies?q=${searchQuery}`
+  //   );
+
+  //   // handle error
+  //   if (!searchResults || !searchResults.ok)
+  //     return handleSearchError(searchResults, response);
+
+  //   const reducedCompanies = Company.filterUKCompanyInformation(
+  //     searchResults?.data?.items
+  //   );
+
+  //   return response.status(200).json(reducedCompanies);
+  // }
+
+  const filterCompanies = (companies: ShortCompany[], searchString: string) => {
+    return companies.filter(company =>
+      company.name.toLowerCase().includes(searchString)
     );
+  };
+  const filteredCompanies = filterCompanies(mockCompanies, searchQuery);
 
-    // handle error
-    if (!searchResults || !searchResults.ok)
-      return handleSearchError(searchResults, response);
+  return response.status(200).json(filteredCompanies);
 
-    const reducedCompanies = Company.filterUKCompanyInformation(
-      searchResults?.data?.items
-    );
-
-    return response.status(200).json(reducedCompanies);
-  }
-
-  return response
-    .status(404)
-    .json({ error: INVALID_COUNTRY_CODE, message: 'Invalid country code.' });
+  // return response
+  //   .status(404)
+  //   .json({ error: INVALID_COUNTRY_CODE, message: 'Invalid country code.' });
 };
 
-const handleSearchError = (
-  results: CompanyResType,
-  response: NextApiResponse
-) => {
-  if (!results || !results.ok) {
-    return response
-      .status(500)
-      .json({
-        error: SEARCH_ERROR,
-        message: 'Error when accessing Companies House API.'
-      });
-  }
-};
+// const handleSearchError = (
+//   results: CompanyResType,
+//   response: NextApiResponse
+// ) => {
+//   if (!results || !results.ok) {
+//     return response
+//       .status(500)
+//       .json({
+//         error: SEARCH_ERROR,
+//         message: 'Error when accessing Companies House API.'
+//       });
+//   }
+// };
 
 export default withSentry(SearchCompanies);
