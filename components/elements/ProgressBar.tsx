d77137e6b49e1e9e7918af6de66eaf2ddedf1a7e
dnirns <danielirons@protonmail.com>
2021-11-02 11:29:09 +0000

    feat: 🎸 progress bar running & responding to variables
    progress bar component working with placeholder variables and running
    inside useEffect on page load - needs mock data adding and to run on
    event handler rather than on load

--- components/elements/ProgressBar.tsx
+++ components/elements/ProgressBar.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useTranslations } from 'use-intl';
 import { TranslateInput } from '../../types/global';
 import Button from './Button';
@@ -9,8 +10,27 @@ interface ProgressBarProps {
 const ProgressBar = ({ buttonText }: ProgressBarProps) => {
   const t = useTranslations();
 
-  // average time of report (variable) 10secs to start
-  //  % to completion
+  // all reports - will come from back end / mock data
+  const totalReports = 127;
+
+  // average time to generate report in ms
+  const averageReportTime = 100;
+
+  // state for total completed reports
+  const [completedReports, setCompletedReports] = useState(0);
+
+  // get % of completed reports rounded to an integer
+  let percentComplete = Math.round((completedReports * 100) / totalReports);
+
+  // run interval on page load - will need to be run via event handler
+  useEffect(() => {
+    let completed = 0;
+    let timer = setInterval(() => {
+      completed += 1;
+      setCompletedReports(completed);
+      completed >= totalReports && clearInterval(timer);
+    }, averageReportTime);
+  }, []);
 
   return (
     <div>
@@ -20,11 +40,18 @@ const ProgressBar = ({ buttonText }: ProgressBarProps) => {
           <p className="pr-1 ">{t('estimated_time_to_completion')}:</p>
           <p>1 mins 36secs</p>
         </div>
-        <p className="font-bold">413/654</p>
+        <p className="font-bold">
+          {completedReports}/{totalReports}
+        </p>
       </div>
 
       <div className="w-full h-3 my-4 rounded-full bg-primary">
-        <div className="h-full w-[80%] bg-highlight rounded-l-full" />
+        <div
+          style={{ width: `${percentComplete}%` }}
+          className={`h-full bg-highlight rounded-l-full ${
+            percentComplete === 100 && 'rounded-r-full'
+          }`}
+        />
       </div>
       <div className="w-1/3 my-4 border border-gray-200 rounded">
         <Button variant="none" disabled>
