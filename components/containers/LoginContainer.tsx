de7d25653f611ade7dae3ea82341ed5eaafb0d22
loyd0 <sam@antler.digital>
2021-11-09 17:46:19 +0400

    fix: 🐛 updated particles to maintained package
    particles on login was maintained, so updated to ts version - also
    updated and removed a few unecessary packages from package json

--- components/containers/LoginContainer.tsx
+++ components/containers/LoginContainer.tsx
@@ -1,4 +1,4 @@
-import Particles from 'react-particles-js';
+import Particles from 'react-tsparticles';
 
 import tailwindConfig from '../../tailwind.config.js';
 
@@ -12,35 +12,54 @@ const LoginContainer = ({ children }: LoginProps) => {
       <Particles
         className="h-full min-h-screen w-full fixed top-0 bg-gray-300"
         params={{
-          particles: {
-            color: { value: tailwindConfig.theme.extend.colors.primary },
-            lineLinked: {
-              color: tailwindConfig.theme.extend.colors.primary
+          fps_limit: 60,
+          interactivity: {
+            detectsOn: 'canvas',
+            events: {
+              onClick: { enable: true, mode: 'push' },
+              onHover: { enable: true, mode: 'repulse' },
+              resize: true
             },
-            number: {
-              value: 50
-            },
-            size: {
-              value: 4
+            modes: {
+              push: { particles_nb: 4 },
+              repulse: { distance: 200, duration: 0.4 }
             }
           },
-          interactivity: {
-            events: {
-              onhover: {
-                enable: true,
-                mode: 'repulse'
-              }
-            }
-          }
+          particles: {
+            color: { value: tailwindConfig.theme.extend.colors.primary },
+            links: {
+              color: tailwindConfig.theme.extend.colors.primary,
+              distance: 150,
+              enable: true,
+              opacity: 0.4,
+              width: 1
+            },
+            move: {
+              bounce: false,
+              direction: 'none',
+              enable: true,
+              outMode: 'out',
+              random: false,
+              speed: 1.5,
+              straight: false
+            },
+            number: { density: { enable: true, area: 800 }, value: 80 },
+            opacity: { value: 0.5 },
+            shape: { type: 'circle' },
+            size: { random: true, value: 5 }
+          },
+          detectRetina: true
         }}
       />
-
-      {/* w-11/12 md:w-136 bg-black text-white flex flex-col mx-auto px-10 md:px-20 py-14 */}
-       <div className={`${children ? 'opacity-100' : 'opacity-0' } transition-opacity duration-500 z-20 w-full px-2`}>
-          <div className="w-full sm:max-w-xl bg-black text-white flex mx-auto flex-col px-12 sm:px-20 py-6 sm:py-14 sm:text-left my-4">
-            {children}
-          </div>
+      <div
+        className={`${
+          children ? 'opacity-100' : 'opacity-0'
+        } transition-opacity duration-500 z-20 w-full px-2`}
+      >
+        <div className="w-full sm:max-w-xl bg-black text-white flex mx-auto flex-col px-12 sm:px-20 py-6 sm:py-14 sm:text-left my-4">
+          {children}
         </div>
+      </div>
     </div>
   );
 };
