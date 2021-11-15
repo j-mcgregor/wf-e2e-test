ec297fecec09918cc21dac4aeaf17b4a897d8fdd
loyd0 <sam@antler.digital>
2021-11-15 20:15:21 +0400

    feat: 🎸 3 updates
    1. implement country codes, make esg %, risk_outlook, pep flags

--- lib/funcs/news.ts
+++ lib/funcs/news.ts
@@ -1,13 +1,44 @@
 import { ApiResType } from '../../types/global';
 
+const validCountryCodes = [
+  'us',
+  'gb',
+  'de',
+  'it',
+  'fr',
+  'nl',
+  'se',
+  'dk',
+  'fi',
+  'hu',
+  'no',
+  'pl',
+  'ru',
+  'ua',
+  'ch',
+  'br',
+  'nz',
+  'mx',
+  'au'
+];
+
 const getCompanyNews = async (
-  companyName: string | undefined
+  companyName: string | undefined,
+  countryCode?: string
 ): Promise<ApiResType> => {
+  const lowerCaseCountryCode = countryCode ? countryCode.toLowerCase() : '';
+  const validCountryCode =
+    countryCode && validCountryCodes.includes(lowerCaseCountryCode)
+      ? countryCode
+      : 'us';
+  const countryCodeString = `&country=${validCountryCode}`;
+
   if (!companyName) {
     return { ok: false };
   }
+
   const res = await fetch(
-    `http://api.datanews.io/v1/headlines?q=${companyName}&size=10`,
+    `http://api.datanews.io/v1/headlines?q=${companyName}${countryCodeString}&size=10`,
     {
       method: 'GET',
       headers: {
