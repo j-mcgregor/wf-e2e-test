4eef69356d57f9646c265e25ee0b80c6ee6bba7b
loyd0 <sam@antler.digital>
2021-11-15 18:21:07 +0400

    updating to public ;

--- config/index.ts
+++ config/index.ts
@@ -1,14 +1,13 @@
 /* eslint-disable no-console */
-const isProduction = process.env.NODE_ENV === 'production';
-const isOnVercel = !!process.env.VERCEL_URL;
+const isOnVercel =
+  !!process.env.NEXT_PUBLIC_VERCEL_URL || !!process.env.VERCEL_URL;
 
 console.log(process.env);
 
 const config = {
-  URL:
-    isProduction && isOnVercel
-      ? `https://${process.env.VERCEL_URL}`
-      : 'http://localhost:3000'
+  URL: isOnVercel
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3000'
 };
 
 export default config;
