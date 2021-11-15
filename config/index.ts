7e66831434e5d76fc5836600e1e469f2c81cd68d
loyd0 <sam@antler.digital>
2021-11-15 18:18:15 +0400

    testing env form vercel

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,8 @@
+/* eslint-disable no-console */
 const isProduction = process.env.NODE_ENV === 'production';
 const isOnVercel = !!process.env.VERCEL_URL;
+
+console.log(process.env);
 
 const config = {
   URL:
