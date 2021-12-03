7d7cb06c73a361b43872003fed8e4812d39b1724
loyd0 <sam@antler.digital>
2021-12-03 14:58:12 +0400

    fix: 🐛 improving the layout of legal events on print

--- components/report-sections/legal-events/LegalRow.tsx
+++ components/report-sections/legal-events/LegalRow.tsx
@@ -26,69 +26,67 @@ const LegalRow = ({
   // }, []);
 
   return (
-    <>
-      <div
-        className={`${textColor} flex flex-col py-1.5 print:border  `}
-        data-testid="legal-row-testid "
-      >
-        <div className="flex">
-          <div className="w-full mr-1">
-            <p>{description}</p>
-          </div>
-          <div className="w-full mr-1">
-            <p>{types?.join(', ')}</p>
-          </div>
-          <div className="w-[230px] flex justify-between">
-            <p>{date}</p>
-
-            <button
-              onClick={handleDropdown}
-              className="text-black cursor-pointer"
-            >
-              {details && !forPrint && (
-                <ChevronUpIcon
-                  className={`${
-                    !dropdownOpen && 'rotate-180'
-                  } w-4 transform duration-300`}
-                />
-              )}
-            </button>
-          </div>
+    <div
+      className={`${textColor} flex flex-col py-1.5 print:border-b print:always-break mb-2  `}
+      data-testid="legal-row-testid "
+    >
+      <div className="flex">
+        <div className="w-full mr-1">
+          <p>{description}</p>
         </div>
-        {details && dropdownOpen && (
-          <div className="w-full my-2 ">
-            {details.map((detail, index) => {
-              return (
-                <div
-                  key={index}
-                  className="flex flex-wrap justify-between bg-bg rounded my-2 "
-                >
-                  {Object.entries(detail).map(keyValueArray => {
-                    const header = camelCaseToSentenceCase(keyValueArray[0]);
-                    const description = keyValueArray[1]
-                      ? camelCaseToSentenceCase(keyValueArray[1])
-                      : 'N/A';
-                    return (
-                      <div
-                        key={header}
-                        className={`flex flex-col px-6 py-4 ${
-                          description?.length > 30 ? 'w-full' : 'w-1/2'
-                        } lg:w-auto`}
-                      >
-                        <p className="py-1">{header}</p>
-                        <p className={`font-semibold print:text-sm`}>
-                          {description}
-                        </p>
-                      </div>
-                    );
-                  })}
-                </div>
-              );
-            })}
-          </div>
-        )}
+        <div className="w-full mr-1">
+          <p>{types?.join(', ')}</p>
+        </div>
+        <div className="w-[230px] flex justify-between">
+          <p>{date}</p>
+
+          <button
+            onClick={handleDropdown}
+            className="text-black cursor-pointer"
+          >
+            {details && !forPrint && (
+              <ChevronUpIcon
+                className={`${
+                  !dropdownOpen && 'rotate-180'
+                } w-4 transform duration-300`}
+              />
+            )}
+          </button>
+        </div>
       </div>
-    </>
+      {details && dropdownOpen && (
+        <div className="w-full my-2 ">
+          {details.map((detail, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-wrap justify-between bg-bg rounded my-2 "
+              >
+                {Object.entries(detail).map(keyValueArray => {
+                  const header = camelCaseToSentenceCase(keyValueArray[0]);
+                  const description = keyValueArray[1]
+                    ? camelCaseToSentenceCase(keyValueArray[1])
+                    : 'N/A';
+                  return (
+                    <div
+                      key={header}
+                      className={`flex flex-col px-6 py-4 ${
+                        description?.length > 30 ? 'w-full' : 'w-1/2'
+                      } lg:w-auto`}
+                    >
+                      <p className="py-1">{header}</p>
+                      <p className={`font-semibold print:text-sm`}>
+                        {description}
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
   );
 };
 
