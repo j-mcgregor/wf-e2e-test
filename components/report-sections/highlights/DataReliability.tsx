7d2da57a684a2d5eaa3009f211cbd5837b9ac332
Jack McGregor <jackjwmcgregor@gmail.com>
2022-01-26 11:07:45 +0000

    fix: 🐛 fixed classname for datareliability (#62)

--- components/report-sections/highlights/DataReliability.tsx
+++ components/report-sections/highlights/DataReliability.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable sonarjs/cognitive-complexity */
+import classNames from 'classnames';
+
 import { useReliability } from '../../../hooks/useReliability';
 import { DataReliabilityType } from '../../../types/report';
 
@@ -20,7 +22,12 @@ const DataReliability = ({
 
   return (
     <div className="sm:w-1/2 md:w-full lg:w-1/2 h-full py-6 print:w-full sm:print:w-full print:flex">
-      <div className={`${styles} border-2 rounded h-1/2 text-sm py-4 px-3`}>
+      <div
+        className={classNames(
+          styles,
+          `border-2 rounded h-1/2 text-sm py-4 px-3`
+        )}
+      >
         <p className="font-bold pb-2">{inBoxTitle}</p>
         <p className="mb-2">{infoBoxText}</p>
         <p>{inBoxExtra}</p>
