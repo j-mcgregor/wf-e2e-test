811914b6040ad65e94e697826623fe0317700975
Jack <jackjwmcgregor@gmail.com>
2022-04-05 15:14:11 -0400

    test: 💍 logging NODE_ENV

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,8 @@ import env from './config';
 dotenv.config({ path: path.resolve(__dirname, '.env.development') });
 
 // eslint-disable-next-line no-console
+console.log(process.env.NODE_ENV);
+// eslint-disable-next-line no-console
 console.log(env);
 
 const config: PlaywrightTestConfig = {
