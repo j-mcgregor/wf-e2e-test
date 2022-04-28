fb705e4051e53e87077c62d9a8132295d7c1c3fd
Jack <jackjwmcgregor@gmail.com>
2022-04-28 16:08:48 -0400

    feat: 🎸 added org name to dashboard title
    hard to test though, since the dev tools don't let me filter a changed
    value through recoil. Will hopefully see it in the PR

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,6 +80,7 @@ export default function Dashboard() {
             <div className="flex justify-between">
               <h1 className="text-2xl font-semibold">
                 {user && user.full_name}
+                {user?.organisation_name && `(${user.organisation_name})`}
               </h1>
             </div>
           </div>
