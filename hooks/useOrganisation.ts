13795a3e41755108153ba13851cdba9ed5ea1167
loyd0 <samloyd@hotmail.co.uk>
2022-05-10 13:19:27 +0400

    fix: 🐛 refactor useOrg to use user details rather than api call

--- hooks/useOrganisation.ts
+++ hooks/useOrganisation.ts
@@ -1,35 +1,37 @@
+import { number } from 'prop-types';
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import useSWR from 'swr';
 
 import appState from '../lib/appState';
 import fetcher from '../lib/utils/fetcher';
-import { OrganisationIndexApi } from '../pages/api/organisation/[orgId]';
 import { OrganisationTypeApi } from '../pages/api/organisation/[orgId]/[type]';
+
+interface OrganisationHookObject {
+  name?: string;
+  id?: string;
+  totalUsers?: number;
+  quota?: {
+    quota_used: number;
+  };
+}
 
 const useOrganisation = (fetch = true) => {
   const { user } = useRecoilValue(appState);
   const orgId = user?.organisation_id || null;
 
-  const { data } = useSWR<OrganisationIndexApi>(
-    fetch && `/api/organisation/${orgId}`,
+  const { data } = useSWR<OrganisationTypeApi>(
+    fetch && orgId && `/api/organisation/${orgId}/users?limit=1`,
     fetcher,
     {
       revalidateOnMount: true
     }
   );
 
-  const { data: result } = useSWR<OrganisationTypeApi>(
-    fetch && `/api/organisation/${orgId}/users?limit=1`,
-    fetcher,
-    {
-      revalidateOnMount: true
-    }
-  );
-
-  const organisation = {
-    ...(data && data?.organisation),
-    totalUsers: (result && result?.total) || 0
+  const organisation: OrganisationHookObject = {
+    name: user?.organisation_name,
+    id: user?.organisation_id,
+    totalUsers: (data && data?.total) || 0
   };
 
   const isLoading = !data;
