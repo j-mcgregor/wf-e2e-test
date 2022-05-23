abc4bea7ba67b2cd1342d42bfcfaa21165efaf2c
jlsfurness@gmail.com <jlsfurness@gmail.com>
2022-05-23 11:48:26 +0100

    refactor: 💡 Added new RadioSelector to add user form

--- components/elements/RadioSelector.tsx
+++ components/elements/RadioSelector.tsx
@@ -11,38 +11,42 @@ interface IRadioSelectorProps {
   options: IRadioOption[];
 }
 
-const RadioSelector: React.FC<IRadioSelectorProps> = ({ name, options }) => {
-  const t = useTranslations();
-  const isFirstOption = (index: number) => index === 0;
-  const isLastOption = (index: number) => index === options.length - 1;
-  return (
-    <ul className="flex w-fit">
-      {options.map(({ label, value }, idx) => (
-        <li className="relative">
-          <input
-            type="radio"
-            name={name}
-            id={`option-${idx}`}
-            className="sr-only peer"
-            value={value}
-            defaultChecked={isFirstOption(idx)}
-          />
-          <label
-            htmlFor={`option-${idx}`}
-            className={`${
-              isFirstOption(idx)
-                ? 'rounded-l-lg'
-                : isLastOption(idx)
-                ? 'rounded-r-lg'
-                : ''
-            } whitespace-nowrap flex h-10 px-4 items-center text-sm justify-center border-[1px] bg-white text-primary peer-checked:border-highlight peer-checked:text-highlight cursor-pointer`}
-          >
-            {t(`${label}`)}
-          </label>
-        </li>
-      ))}
-    </ul>
-  );
-};
+const RadioSelector = React.forwardRef<HTMLInputElement, IRadioSelectorProps>(
+  ({ name, options, ...otherProps }, ref) => {
+    const t = useTranslations();
+    const isFirstOption = (index: number) => index === 0;
+    const isLastOption = (index: number) => index === options.length - 1;
+    return (
+      <ul className="flex w-full">
+        {options.map(({ label, value }, idx) => (
+          <li className="relative" key={`${name}-option-${idx}`}>
+            <input
+              ref={ref}
+              type="radio"
+              name={name}
+              id={`option-${idx}`}
+              className="sr-only peer"
+              value={value}
+              defaultChecked={isFirstOption(idx)}
+              {...otherProps}
+            />
+            <label
+              htmlFor={`option-${idx}`}
+              className={`${
+                isFirstOption(idx)
+                  ? 'rounded-l-lg'
+                  : isLastOption(idx)
+                  ? 'rounded-r-lg'
+                  : ''
+              } whitespace-nowrap flex h-10 px-4 items-center text-sm justify-center border-[1px] bg-white text-primary peer-checked:border-highlight peer-checked:text-highlight cursor-pointer`}
+            >
+              {t(`${label}`)}
+            </label>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+);
 
 export default RadioSelector;
