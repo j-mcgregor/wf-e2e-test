b37d3f27efbe1551c8665e147a8cf1c290d12a87
Jack <jackjwmcgregor@gmail.com>
2022-05-26 16:10:09 -0400

    ci: 🎡 removed auth json, updated gitignore, reset baseURL

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ const config: PlaywrightTestConfig = {
   outputDir: './playwright/test-results',
   use: {
     headless: true,
-    baseURL: 'https://wiserfunding-platform-o89wwcjoy-wiserfunding.vercel.app',
+    baseURL: env.URL,
     viewport: { width: 1400, height: 980 },
     browserName: 'chromium',
     screenshot: 'only-on-failure',
