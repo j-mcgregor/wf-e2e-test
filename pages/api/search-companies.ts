a0d3ad9d38a44ed17c027e9b5667a3fd466a674a
jlsfurness@gmail.com <jlsfurness@gmail.com>
2022-06-13 15:12:49 +0100

    refactor: 💡 Converted search-companies to APIHandler

--- pages/api/search-companies.ts
+++ pages/api/search-companies.ts
@@ -11,104 +11,210 @@ import {
 } from '../../lib/utils/error-codes';
 import { ApiError, ApiResType } from '../../types/global';
 
-import type { NextApiHandler, NextApiResponse } from 'next';
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import { getToken } from 'next-auth/jwt';
+import APIHandler from '../../lib/api-handler/handler';
+import authenticators from '../../lib/api-handler/authenticators';
 
 type UkCompanyFilter = ReturnType<typeof Company.filterUKCompanyInformation>;
 type EuCompanyFilter = ReturnType<
   typeof Company.mapEUCompanyDataToResponseFormat
 >;
 
-// Declaring function for readability with Sentry wrapper
-const searchCompaniesApi: NextApiHandler<
-  UkCompanyFilter | EuCompanyFilter | ApiError
-> = async (request, response) => {
-  const token = await getToken({
-    req: request,
-    secret: `${process.env.NEXTAUTH_SECRET}`
-  });
+const searchCompaniesApi = async (
+  request: NextApiRequest,
+  response: NextApiResponse
+) => {
+  APIHandler(request, response, {
+    config: {
+      sourceType: 'SEARCH_COMPANIES',
+      authenticate: authenticators.NextAuth
+    },
+    GET: async ({ query }) => {
+      const searchQuery: string | undefined = query?.query
+        ?.toString()
+        ?.toLowerCase();
+      const countryCode: string | undefined = query?.country
+        ?.toString()
+        ?.toLowerCase();
 
-  // unauthenticated requests
-  if (!token) {
-    return response.status(403).json({
-      error: UNAUTHORISED,
-      message: 'Unauthorised api request, please login to continue.'
-    } as ApiError);
-  }
+      if (!countryCode) {
+        return {
+          defaultResponse: {
+            status: 404,
+            code: 'COUNTRY_CODE_REQUIRED',
+            message: 'Country code is required.'
+          }
+        };
+      }
 
-  // extract search query
-  const searchQuery: string = request.query.query?.toString()?.toLowerCase();
+      if (!searchQuery) {
+        return {
+          defaultResponse: {
+            status: 200,
+            ok: true,
+            code: 'SEARCH_COMPANIES_SUCCESS',
+            message: 'No search query provided.',
+            data: []
+          }
+        };
+      }
 
-  // country code required for adding different jurisdictions
-  const countryCode: string = request.query?.country?.toString()?.toLowerCase();
+      if (countryCode === 'gb') {
+        const searchResults = await Company.searchUKCompaniesHouse(
+          process.env.COMPANIES_HOUSE_BASE64_KEY,
+          `companies?q=${searchQuery}`
+        );
 
-  if (!countryCode) {
-    return response.status(404).json({
-      error: COUNTRY_CODE_REQUIRED,
-      message:
-        'Please provide a country code in order to correctly access the right database'
-    } as ApiError);
-  }
+        const reducedCompanies = Company.filterUKCompanyInformation(
+          searchResults?.data?.items
+        );
 
-  if (!searchQuery) {
-    return response.status(200).json([]);
-  }
+        return {
+          defaultResponse: {
+            status: searchResults.ok ? 200 : 404,
+            ok: searchResults.ok || false,
+            code: searchResults.ok
+              ? 'SEARCH_COMPANIES_SUCCESS'
+              : 'SEARCH_COMPANIES_ERROR',
+            message: searchResults.ok ? 'Search successful.' : 'Search failed.',
+            data: reducedCompanies
+          }
+        };
+      } else if (
+        countryCode &&
+        orbisAvailableSearchCountries.includes(countryCode)
+      ) {
+        // search orbis for matching companies in the country
+        const searchResults = await Company.SearchOrbisCompanies(
+          process.env.ORBIS_SEARCH_API_KEY,
+          searchQuery,
+          countryCode
+        );
 
-  if (countryCode === 'gb') {
-    const searchResults = await Company.searchUKCompaniesHouse(
-      process.env.COMPANIES_HOUSE_BASE64_KEY,
-      `companies?q=${searchQuery}`
-    );
+        // filter out those without a BVDID
+        // reduce to a consistent format
+        const reducedCompanies = Company.filterAndReduceEUCompanyInformation(
+          searchResults?.data
+        );
 
-    //  handle error
-    if (!searchResults || !searchResults.ok) {
-      return handleSearchError(searchResults, response);
+        // map to the required format to return
+        const mappedCompanies = Company.mapEUCompanyDataToResponseFormat(
+          reducedCompanies,
+          countryCode
+        );
+
+        return {
+          defaultResponse: {
+            status: 200,
+            code: 'SEARCH_COMPANIES_SUCCESS',
+            message: 'Search successful.',
+            data: mappedCompanies
+          }
+        };
+      } else {
+        return {
+          defaultResponse: {
+            status: 404,
+            code: 'INVALID_COUNTRY_CODE',
+            message: 'Invalid country code.'
+          }
+        };
+      }
     }
-
-    const reducedCompanies = Company.filterUKCompanyInformation(
-      searchResults?.data?.items
-    );
-
-    return response.status(200).json(reducedCompanies);
-  } else if (
-    countryCode &&
-    orbisAvailableSearchCountries.includes(countryCode)
-  ) {
-    // search orbis for matching companies in the country
-    const searchResults = await Company.SearchOrbisCompanies(
-      process.env.ORBIS_SEARCH_API_KEY,
-      searchQuery,
-      countryCode
-    );
-
-    // filter out those without a BVDID
-    // reduce to a consistent format
-    const reducedCompanies = Company.filterAndReduceEUCompanyInformation(
-      searchResults?.data
-    );
-
-    // map to the required format to return
-    const mappedCompanies = Company.mapEUCompanyDataToResponseFormat(
-      reducedCompanies,
-      countryCode
-    );
-
-    return response.status(200).json(mappedCompanies);
-  } else {
-    return response.status(404).json({
-      error: INVALID_COUNTRY_CODE,
-      message: 'Invalid country code.'
-    } as ApiError);
-  }
+  });
 };
 
-const handleSearchError = (results: ApiResType, response: NextApiResponse) => {
-  if (!results || !results.ok) {
-    return response.status(500).json({
-      error: SEARCH_ERROR,
-      message: 'Error when accessing search API.'
-    } as ApiError);
-  }
-};
+// Declaring function for readability with Sentry wrapper
+// const searchCompaniesApi: NextApiHandler<
+//   UkCompanyFilter | EuCompanyFilter | ApiError
+// > = async (request, response) => {
+//   const token = await getToken({
+//     req: request,
+//     secret: `${process.env.NEXTAUTH_SECRET}`
+//   });
+
+//   // unauthenticated requests
+//   if (!token) {
+//     return response.status(403).json({
+//       error: UNAUTHORISED,
+//       message: 'Unauthorised api request, please login to continue.'
+//     } as ApiError);
+//   }
+
+//   // extract search query
+//   const searchQuery: string = request.query.query?.toString()?.toLowerCase();
+
+//   // country code required for adding different jurisdictions
+//   const countryCode: string = request.query?.country?.toString()?.toLowerCase();
+
+//   if (!countryCode) {
+//     return response.status(404).json({
+//       error: COUNTRY_CODE_REQUIRED,
+//       message:
+//         'Please provide a country code in order to correctly access the right database'
+//     } as ApiError);
+//   }
+
+//   if (!searchQuery) {
+//     return response.status(200).json([]);
+//   }
+
+//   if (countryCode === 'gb') {
+//     const searchResults = await Company.searchUKCompaniesHouse(
+//       process.env.COMPANIES_HOUSE_BASE64_KEY,
+//       `companies?q=${searchQuery}`
+//     );
+
+//     //  handle error
+//     if (!searchResults || !searchResults.ok) {
+//       return handleSearchError(searchResults, response);
+//     }
+
+//     const reducedCompanies = Company.filterUKCompanyInformation(
+//       searchResults?.data?.items
+//     );
+
+//     return response.status(200).json(reducedCompanies);
+//   } else if (
+//     countryCode &&
+//     orbisAvailableSearchCountries.includes(countryCode)
+//   ) {
+//     // search orbis for matching companies in the country
+//     const searchResults = await Company.SearchOrbisCompanies(
+//       process.env.ORBIS_SEARCH_API_KEY,
+//       searchQuery,
+//       countryCode
+//     );
+
+//     // filter out those without a BVDID
+//     // reduce to a consistent format
+//     const reducedCompanies = Company.filterAndReduceEUCompanyInformation(
+//       searchResults?.data
+//     );
+
+//     // map to the required format to return
+//     const mappedCompanies = Company.mapEUCompanyDataToResponseFormat(
+//       reducedCompanies,
+//       countryCode
+//     );
+
+//     return response.status(200).json(mappedCompanies);
+//   } else {
+//     return response.status(404).json({
+//       error: INVALID_COUNTRY_CODE,
+//       message: 'Invalid country code.'
+//     } as ApiError);
+//   }
+// };
+
+// const handleSearchError = (results: ApiResType, response: NextApiResponse) => {
+//   if (!results || !results.ok) {
+//     return response.status(500).json({
+//       error: SEARCH_ERROR,
+//       message: 'Error when accessing search API.'
+//     } as ApiError);
+//   }
+// };
 
 export default withSentry(searchCompaniesApi);
