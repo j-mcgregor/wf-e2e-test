f2945d7398278835f346da5ba97c6285218476a3
jlsfurness@gmail.com <jlsfurness@gmail.com>
2022-06-14 08:37:59 +0100

    refactor: 💡 Updated reset-password endpoint

--- pages/api/password-reset.ts
+++ pages/api/password-reset.ts
@@ -9,65 +9,120 @@ import { StatusCodeConstants } from '../../types/http-status-codes';
 
 import type { NextApiHandler } from 'next';
 import { VALID_PASSWORD } from '../../lib/utils/regexes';
+import APIHandler from '../../lib/api-handler/handler';
+import authenticators from '../../lib/api-handler/authenticators';
+import { fetchWrapper } from '../../lib/utils/fetchWrapper';
 
 const { BAD_REQUEST, METHOD_NOT_ALLOWED } = StatusCodeConstants;
 
-export interface PasswordResetApi extends ForgotPassword, ResetPassword {}
+const XMLHeaders = {
+  'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+};
 
-// Declaring function for readability with Sentry wrapper
-const passwordReset: NextApiHandler<PasswordResetApi> = async (
-  request,
-  response
-) => {
-  const { email } = request.query;
-  const { token, newPassword } = request.body;
-  switch (request.method) {
-    case 'GET':
-      /** @action REQUEST EMAIL TO CHANGE EMAIL ADDRESS  */
-      if (email && typeof email === 'string') {
-        const result = await User.forgotPassword(email, {});
-
-        return response.status(result.status).json(result);
+const passwordReset: NextApiHandler = async (request, response) => {
+  APIHandler(request, response, {
+    config: {
+      sourceType: 'PASSWORD_RESET',
+      authenticate: authenticators.NextAuth,
+      publicMethods: {
+        GET: true,
+        POST: true
       }
+    },
+    GET: async ({ query }) => {
+      try {
+        const { email } = query;
 
-      return response.status(BAD_REQUEST).json({
-        ...makeApiHandlerResponseFailure({
-          message: errorsBySourceType.GENERAL[BAD_REQUEST]
-        }),
-        msg: null
-      });
-    case 'POST':
-      if (token && newPassword) {
-        /* @TODO update with new error handling */
-        if (!VALID_PASSWORD.test(newPassword)) {
-          return response.status(BAD_REQUEST).json({
-            ...makeApiHandlerResponseFailure({
-              message: errorsBySourceType.GENERAL[BAD_REQUEST]
-            }),
-            msg: null
-          });
+        if (email && typeof email === 'string') {
+          return {
+            response: await fetchWrapper(
+              `${process.env.WF_AP_ROUTE}/password-recovery/${email}`,
+              {
+                method: 'POST',
+                headers: {
+                  ...XMLHeaders
+                }
+              }
+            )
+          };
         }
-        const result: ResetPassword = await User.resetPassword(token, {
-          newPassword
-        });
-
-        return response.status(200).json(result);
+        return {
+          defaultResponse: {
+            status: 400,
+            code: 'EMAIL_REQUIRED',
+            message: 'Email is required.'
+          }
+        };
+      } catch (e) {
+        return {
+          defaultResponse: {
+            status: 500,
+            code: 'PASSWORD_RESET_FAILURE',
+            message: 'Password reset failed.'
+          }
+        };
       }
+    },
+    POST: async ({ body }) => {
+      try {
+        const { token, newPassword } = body as {
+          token?: string;
+          newPassword?: string;
+        };
 
-      return response.status(BAD_REQUEST).json({
-        ...makeApiHandlerResponseFailure({
-          message: errorsBySourceType.USER[BAD_REQUEST]
-        }),
-        msg: null
-      });
-    default:
-      return response.status(METHOD_NOT_ALLOWED).json({
-        ...makeApiHandlerResponseFailure({
-          message: errorsBySourceType.GENERAL[METHOD_NOT_ALLOWED]
-        }),
-        msg: null
-      });
-  }
+        if (!token) {
+          return {
+            defaultResponse: {
+              status: 400,
+              code: 'TOKEN_REQUIRED',
+              message: 'Token is required.'
+            }
+          };
+        }
+
+        if (!newPassword) {
+          return {
+            defaultResponse: {
+              status: 400,
+              code: 'NEW_PASSWORD_REQUIRED',
+              message: 'New password is required.'
+            }
+          };
+        }
+
+        if (token && newPassword) {
+          if (!VALID_PASSWORD.test(newPassword)) {
+            return {
+              defaultResponse: {
+                status: 422,
+                code: 'INVALID_PASSWORD',
+                message:
+                  'Password must be at least 8 characters long and contain at least one number, one uppercase, one lowercase letter and one special character.'
+              }
+            };
+          }
+
+          return {
+            response: await fetchWrapper(
+              `${process.env.WF_AP_ROUTE}/reset-password/`,
+              {
+                method: 'POST',
+                body: JSON.stringify({ token, new_password: newPassword })
+              }
+            )
+          };
+        }
+      } catch (e) {
+        return {
+          defaultResponse: {
+            status: 500,
+            code: 'PASSWORD_RESET_FAILURE',
+            message: 'Password reset failed.'
+          }
+        };
+      }
+    }
+  });
 };
 
 export default withSentry(passwordReset);
