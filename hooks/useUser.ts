56d045893f8ced3ddc2f63abeef3c25011f65a09
loyd0 <samloyd@hotmail.co.uk>
2022-06-15 08:44:37 +0200

    fix: 🐛 update useUserHook to take data from session

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,13 +1,17 @@
+import { useSession } from 'next-auth/react';
 import React from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import useSWR from 'swr';
 import appState from '../lib/appState';
 import fetcher from '../lib/utils/fetcher';
 import { UserIndexApi } from '../pages/api/user';
+import { UserType } from '../types/global';
 
 const useUser = (fetch: boolean = true) => {
   const { user } = useRecoilValue(appState);
   const setState = useSetRecoilState(appState);
+
+  const { data: sessionUser } = useSession();
 
   // if no user then revalidate onMount to prevent blank page
   const { data, isValidating } = useSWR<UserIndexApi>(
@@ -18,13 +22,13 @@ const useUser = (fetch: boolean = true) => {
       revalidateOnFocus: false
     }
   );
-
+  const is_sso = sessionUser?.user?.is_sso;
   const isLoading = !data;
   React.useEffect(() => {
     if (data?.user && !isValidating) {
-      setState({ ...appState, user: { ...data?.user } });
+      setState({ ...appState, user: { ...data?.user, is_sso } });
     }
-  }, [data]);
+  }, [data, sessionUser]);
 
   const isAdmin = user?.organisation_role === 'Admin';
 
