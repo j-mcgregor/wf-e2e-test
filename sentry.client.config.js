fe9e6c38ab2704ba07f8b2d6dca8811a978375a9
loyd0 <samloyd@hotmail.co.uk>
2022-07-21 14:26:21 +0200

    fix: 🐛 update sentry configs to represent the env correctly

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -13,7 +13,14 @@ if (process.env.NODE_ENV === 'production') {
       'https://1570e51fee894af7a30df4019d41d0f7@o1006047.ingest.sentry.io/5966515',
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 0.2,
-    environment: 'production'
+    environment:
+      process.env.NEXT_PUBLIC_GIT_BRANCH === 'main'
+        ? 'production'
+        : process.env.NEXT_PUBLIC_GIT_BRANCH === 'prerelease'
+        ? 'staging'
+        : process.env.NEXT_PUBLIC_GIT_BRANCH === 'development'
+        ? 'development'
+        : 'local'
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
     // that it will also get attached to your source maps
